bb60c7be69649e23bf86e28f73591625e4876cd5
aiyuhang <aiyuhang@wityun.com>
2020-08-05 00:45:36 +0800

    change 4.1 finished 5.1

--- 4.1/basic_read_write.rb
+++ 4.1/basic_read_write.rb
@@ -1,26 +1,40 @@
-class BaseReadWrite
-  def initialize
-  end
+# writes the number of lines then each line as a string.
 
-  def self.main
-    aFile = File.new("text.txt", "a+")
-    File.truncate("text.txt", 0)
-    if aFile
-      aFile.syswrite("Fred\n")
-      aFile.syswrite("Sam\n")
-      aFile.syswrite("Jill\n")
-      aFile.syswrite("Jenny\n")
-      aFile.syswrite("Zorro\n")
-    else
-      puts "Unable to open file!"
-    end
-    aFile.close
-
-    arrs = IO.readlines("text.txt")
-    arrs.size.times do |arr|
-      p arrs[arr].chomp
-    end
-  end
+def write_data_to_file(a_file)
+  a_file.puts('5')
+  a_file.puts('Fred')
+  a_file.puts('Sam')
+  a_file.puts('Jill')
+  a_file.puts('Jenny')
+  a_file.puts('Zorro')
 end
 
-BaseReadWrite.main
+# reads in each line.
+# you need to change the following code
+# so that it uses a loop which repeats
+# acccording to the number of lines in the File
+# which is given in the first line of the File
+def read_data_from_file(a_file)
+ count = a_file.gets.to_i
+ puts count.to_s
+ 5.times do
+  puts a_file.gets
+ end
+end
+
+# writes data to a file then reads it in and prints
+# each line as it reads.
+# you should improve the modular decomposition of the
+# following by moving as many lines of code
+# out of main as possible.
+def main
+ a_file = File.new("mydata.txt", "w") # open for writing
+ write_data_to_file(a_file)
+ a_file.close
+
+ a_file = File.new("mydata.txt", "r") # open for reading
+ read_data_from_file(a_file)
+ a_file.close
+end
+
+main
